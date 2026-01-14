b8e9da6ac57a63006d471299917d8248d91bc137
delanoismynameo <145506142+delanoismynameo@users.noreply.github.com>
2026-01-14 13:12:48 -0700

    Update

---
+++ prove/mission/mission.js
@@ -0,0 +1,14 @@
+let selectElem = document.querySelector('#mode_select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        // code for changes to colors and logo
+    } else {
+        // code for changes to colors and logo
+    }
+}           
+                    
