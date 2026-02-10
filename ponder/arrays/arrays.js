aaad4b9b1ae2ed7dce1651c9aecd9b32770757b7
delanoismynameo <ste20059@byui.edu>
2026-02-09 20:45:55 -0700

    qwerty

---
+++ ponder/arrays/arrays.js
@@ -0,0 +1,83 @@
+let pets = ['goldfish', 'dog', 'rhino'];
+
+console.log(pets[2]);
+console.log(pets.length);
+
+pets[3]='bunny';
+
+console.log(pets);
+
+pets.push('lizard');
+
+console.log(pets);
+
+const steps = ['one', 'two', 'three'];
+
+steps.forEach(function(item){
+    console.log(item);
+})
+
+steps.forEach(showSteps);
+
+function showSteps(item){
+    console.log(item);
+}
+
+let myList = document.querySelector('#myList');
+
+const stepsHtml = steps.map(listTemplate);
+
+function listTemplate(item) {
+    return `<li>${item}</li>`
+}
+myList.innerHTML = stepsHtml.join('');
+
+let grades = ['A', 'B', 'D']
+
+let points;
+
+let gpaPoints = grades.map(convert);
+
+function convert(grade) {
+    switch (grade.toUpperCase()) {
+        case 'A':
+            points = 4;
+            break;
+        case 'B':
+            points = 3;    
+            break;
+        case 'C':
+            points = 2;
+            break;
+        case 'D':
+            points = 1;
+            break;
+        case 'F':
+            points = 0;
+            break;
+        default:
+            alert('not a valid grade');
+    }
+    return points;
+}
+console.log(gpaPoints);
+console.log(grades);
+
+let totalPoints = gpaPoints.reduce(getTotal);
+
+function getTotal(total, item) {
+    return total + item;
+}
+
+console.log(totalPoints);
+let gpaAverage = totalPoints/gpaPoints.length;
+
+console.log(gpaAverage);
+
+
+const words = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
+
+const shortWords = words.filter(function(word){
+    return word.length < 6;
+})
+console.log(shortWords);
