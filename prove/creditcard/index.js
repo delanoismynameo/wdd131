ed3c9c89ff3dc9b0d4fba596a765c4a8784bfb42
delanoismynameo <ste20059@byui.edu>
2026-02-25 11:10:38 -0700

    asdfg

--- prove/creditcard/index.js
+++ prove/creditcard/index.js
@@ -16,34 +16,31 @@ function submitHandler(event) {
     displayError('');
 
     const cardNum = cardNumber.value.trim();
-    if (paymentSelect.value === 'creditCard') {
-        // Check if it's numeric and valid in one go
-        
-        if (!/^\d{16}$/.test(cardNum)) {
-        errorMsg += 'Card number must be 16 digits\n';
-        } else if (!isCardNumberValid(cardNum)) {
-            errorMsg += 'Card number is not valid\n';
-        }
-        
-        //check date
-        const expYear = Number(document.querySelector('#year').value);   
-        const expMonth = Number(document.querySelector('#month').value);
-        const currentDate = new Date()
+    if (!/^\d{16}$/.test(cardNum)) {
+    errorMsg += 'Card number must be 16 digits\n';
+    } else if (!isCardNumberValid(cardNum)) {
+        errorMsg += 'Card number is not valid\n';
+    }
+    
+    //check date
+    const expYear = Number(document.querySelector('#year').value);   
+    const expMonth = Number(document.querySelector('#month').value);
+    const currentDate = new Date()
 
-        if (2000 + expYear < currentDate.getFullYear() || (2000 + expYear === currentDate.getFullYear() && expMonth <= (currentDate.getMonth()))
-        ) {
-            errorMsg += 'Card is expired\n';
-        }
+    if (2000 + expYear < currentDate.getFullYear() || (2000 + expYear === currentDate.getFullYear() && expMonth <= (currentDate.getMonth()))
+    ) {
+        errorMsg += 'Card is expired\n';
     }
 
-        if (errorMsg !== '') {
-            // there was an error. stop the form and display the errors.
-            displayError(errorMsg)
-            return;
-        }
-        // Success: show a confirmation message
-        const formContainer = document.getElementById('checkoutForm');
-        formContainer.innerHTML = '<h2>Thank you for your purchase.</h2>';
+    if (errorMsg !== '') {
+        // there was an error. stop the form and display the errors.
+        displayError(errorMsg)
+        return;
+    }
+    // Success: show a confirmation message
+    const formContainer = document.getElementById('creditForm');
+    formContainer.classList.add('success-mode');
+    formContainer.innerHTML = '<h2>You di it!</h2>';
 }
 
-document.querySelector('#checkoutForm').addEventListener('submit', submitHandler);
+document.querySelector('#creditForm').addEventListener('submit', submitHandler);
